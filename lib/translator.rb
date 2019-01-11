9fd1ba1767e8f6ea9399c05673bf386713443c13
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-01-11 01:55:27 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -10,7 +10,7 @@ def load_library(file_path)
   dict
 end
 
-def get_japanese_emoticon(yaml, emoticon)
+def get_japanese_emoticon(yaml_file, emoticon)
   load_library(yaml)['get_emoticon'][emoticon]
 end
 
