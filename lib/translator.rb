178c07deabc23e3939cd813fee7e603ceebfcc83
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-01-11 02:08:51 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,6 +23,7 @@ def get_japanese_emoticon(yaml_file, emoticon)
 end
 
 def get_english_meaning(yaml_file, emoticon)
+  binding.pry
   meaning = load_library(yaml_file)['meaning'][emoticon]
   apology = 'Sorry, that emoticon was not found'
   if meaning == nil
