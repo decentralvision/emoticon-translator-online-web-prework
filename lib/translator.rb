6704ef975dbdd77830199efe4d3eb43790a324f9
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-01-11 01:51:42 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,8 @@ def load_library(file_path)
   dict = {get_meaning => {}, get_emoticon => {}}
   yaml = YAML.load_file(file_path)
   yaml.each do |key, hash|
-    dict[]
+    dict[get_meaning][hash[1]]
+  end
 end
 
 def get_japanese_emoticon
